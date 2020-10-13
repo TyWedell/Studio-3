392045b6783b3d9f3bccb3343d78e2427a3f8d65
TyWedell <tywedell@gmail.com>
2020-10-13 17:16:08 -0500

    Menu and Menu Item bases

---
+++ RestaurantMenu/RestaurantMenu/Menu.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RestaurantMenu
+{
+    class Menu
+    {
+        private DateTime lastChanged;
+        private List<MenuItem> AllMenutItems = new List<MenuItem>();
+        private List<MenuItem> appetizers = new List<MenuItem>();
+        private List<MenuItem> mainCourses = new List<MenuItem>();
+        private List<MenuItem> desserts = new List<MenuItem>();
+
+        public List<MenuItem> AllMenuItems { get; set; }
+
+        private void SortMenuItems (List<MenuItem> menuItems)
+        {
+            foreach (MenuItem menuItem in menuItems)
+            {
+                if( menuItem.Category == "appetizer")
+                {
+                    appetizers.Add(menuItem);
+                }
+                else if ( menuItem.Category == "mainCourse")
+                {
+                    mainCourses.Add(menuItem);
+                }
+                else if ( menuItem.Category == "dessert")
+                {
+                    desserts.Add(menuItem);
+                }
+            }
+        }
+
+    }
+}
---
+++ RestaurantMenu/RestaurantMenu/MenuItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace RestaurantMenu
+{
+    class MenuItem
+    {
+        private string category;
+        private string description;
+        private float price;
+        private bool isNew;
+
+        public string Category { get { return category; } }
+        public string Description { get { return description; } }
+        public float Price { get { return price; } }
+        public bool IsNew { get { return isNew; } }
+
+        public MenuItem (float price, string description, string category, bool isNew = true)
+        {
+            this.price = price;
+            this.description = description;
+            this.category = category;
+            this.isNew = isNew;
+        }
+    }
+}
